8e3d2c4be13e72f67fce17d1cddb6a282d13f871
xieqj <xqj198404@gmail.com>
2014-11-21 17:28:00 +0800

    201411211727

--- WebProvider/Dao/InfoDao.cs
+++ WebProvider/Dao/InfoDao.cs
@@ -3,19 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Webs.Model;
 
 namespace Webs.Dao
 {
     internal class InfoDao
     {
-        internal static List<Model.Info> GetListByChannelId(int channelId)
+        internal static List<Info> GetListByChannelId(int channelId)
         {
-            throw new NotImplementedException();
+            List<Info> list = new List<Info>();
+            StringBuilder sqlCmd = new StringBuilder("SELECT a.ShowSort,a.InfoTitle,a.InfoContent,a.TitleImg,a.`Enable`,a.CreateTime,a.ModifyTime,a.CreateBy,a.ModifyBy,a.IsDelete,b.InfoId FROM Info AS a INNER JOIN Channel_Info_Relation AS b ON a.InfoId = b.InfoId WHERE a.IsDelete=0 and a.Enable=1 and b.ChannelId =");
+            sqlCmd.Append(channelId.ToString());
+            using (var dr = MysqlHelper.ExcuteReader(sqlCmd.ToString()))
+            {
+                while (dr.Read())
+                {
+                    Info data = new Info()
+                    {
+                        InfoContent = dr["InfoTitle"].ToString(),
+                        ShowSort = dr.GetInt32("ShowSort"),
+                        InfoId = dr.GetInt32("InfoId"),
+                        InfoTitle = dr["InfoTitle"].ToString(),
+                        CreateBy = dr.GetInt32("CreateBy"),
+                        CreateTime = dr.GetDateTime("CreateTime"),
+                        Enable = dr.GetBoolean("Enable"),
+                        ModifyBy = dr.GetInt32("ModifyBy"),
+                        ModifyTime = dr.GetDateTime("ModifyTime"),
+                        TitleImg = dr["TitleImg"].ToString(),
+
+                    };
+                    list.Add(data);
+                }
+            }
+            if (list.Count == 0) list = null;
+            return list;
         }
 
         internal static Model.Info GetInfoById(int id)
         {
-            throw new NotImplementedException();
+            StringBuilder sqlCmd = new StringBuilder("SELECT * FROM Info WHERE IsDelete=0 and Enable=1 and InfoId =");
+            sqlCmd.Append(id.ToString());
+            Info data = null;
+            using (var dr = MysqlHelper.ExcuteReader(sqlCmd.ToString()))
+            {
+                if (dr.Read())
+                {
+                    data = new Info()
+                   {
+                       InfoContent = dr["InfoTitle"].ToString(),
+                       ShowSort = dr.GetInt32("ShowSort"),
+                       InfoId = dr.GetInt32("InfoId"),
+                       InfoTitle = dr["InfoTitle"].ToString(),
+                       CreateBy = dr.GetInt32("CreateBy"),
+                       CreateTime = dr.GetDateTime("CreateTime"),
+                       Enable = dr.GetBoolean("Enable"),
+                       ModifyBy = dr.GetInt32("ModifyBy"),
+                       ModifyTime = dr.GetDateTime("ModifyTime"),
+                       TitleImg = dr["TitleImg"].ToString(),
+
+                   };
+                }
+            }
+            return data;
         }
     }
 }
