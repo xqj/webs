fa17e4a5275f98b9af013437df7ad1bc60211af0
xieqj <xqj198404@gmail.com>
2014-11-24 09:54:45 +0800

    添加单元测试

---
+++ WebProviderTests/Provider/SecurityProviderTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webs.Provider;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Webs.Provider.Tests
+{
+    [TestClass()]
+    public class SecurityProviderTests
+    {
+        [TestMethod()]
+        public void LoginTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
